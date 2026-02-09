f0ed2e5b4bbd7654ebcb1190a1cae3dadbdac9a1
ArthurYang80 <arthur.yang@student.tdsb.on.ca>
2026-02-09 14:24:33 +0000

    Completed

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -13,38 +13,87 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
-		
+		Scanner s = new Scanner(System.in);
+		System.out.println("In: ");
+		s.nextLine();		
+		String word = s.nextLine();
+		if (word.length() > 0) {
+			System.out.println(word.charAt(0));
+		} else {
+			System.out.println();
+		}
+		s.close();
 	}
 
 	public static void q2() {
-		//Write question 2 code here
-		
+		Scanner s = new Scanner(System.in);
+		System.out.println("In: ");
+		boolean bool = s.nextBoolean();
+		System.out.println(!bool);
+		s.close();
 	}
 
 	public static void q3() {
-		//Write question 3 code here
-		
-	}
+		Scanner s = new Scanner(System.in);	
+	    int num = s.nextInt();
+	    if(num > 5) {
+			System.out.println("true");
+	    } else { 	
+			System.out.println("false");
+	    }
+		s.close();
+}
 
 	public static void q4() {
-		//Write question 4 code here
-		
+		Scanner s = new Scanner(System.in);
+		System.out.println("In: ");
+		double num = s.nextDouble();	
+		if (num % -2 == 2) {
+			System.out.println("True");
+		} else {
+			System.out.println("False");
+		}
+		s.close();	
 	}
 
 	public static void q5() {
-		//Write question 5 code here
-		
+		Scanner s = new Scanner(System.in);
+		System.out.println("In: ");
+		String word = s.nextLine();
+		if (word.equals( "Hello World")) {
+			System.out.println("True");
+		} else {
+			System.out.println("False");
+		}
+		s.close();
 	}
 
 	public static void q6() {
-		//Write question 6 code here
-		
+		Scanner s = new Scanner(System.in);	
+		System.out.println("In: ");
+		int num = s.nextInt();
+		System.out.println("In: ");
+		int num2 = s.nextInt();
+		if (num <= num2) {
+			System.out.println("True");
+		}	else {
+			System.out.println("False");
+		}
+		s.close();
 	}
 
 	public static void q7() {
-		//Write question 7 code here
-		
-	}
-
+		Scanner s = new Scanner(System.in);
+		System.out.println("In: ");
+		double num = s.nextDouble();
+		System.out.println("In: ");
+		double num2 = s.nextDouble();
+		if (num > num2) {
+			System.out.println("True");
+		}	else {
+			System.out.println("False");
+		}
+		s.close();
+		}	
 }
+
