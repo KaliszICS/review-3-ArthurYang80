3a2479abf0427d6652fc570a08d52bc7945d1499
ArthurYang80 <342802865@tdsb.ca>
2026-02-14 08:13:26 +0000

    Finalized

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -14,8 +14,7 @@ public class PracticeProblem {
 
 	public static void q1() {
 		Scanner s = new Scanner(System.in);
-		System.out.println("In: ");
-		s.nextLine();		
+		System.out.print("In: ");
 		String word = s.nextLine();
 		if (word.length() > 0) {
 			System.out.println(word.charAt(0));
@@ -27,71 +26,72 @@ public class PracticeProblem {
 
 	public static void q2() {
 		Scanner s = new Scanner(System.in);
-		System.out.println("In: ");
+		System.out.print("In: ");
 		boolean bool = s.nextBoolean();
 		System.out.println(!bool);
 		s.close();
 	}
 
 	public static void q3() {
-		Scanner s = new Scanner(System.in);	
-	    int num = s.nextInt();
+		Scanner s = new Scanner(System.in);
+		System.out.print("In: ");
+		int num = s.nextInt();
 	    if(num > 5) {
 			System.out.println("true");
 	    } else { 	
 			System.out.println("false");
 	    }
 		s.close();
-}
+	}
 
 	public static void q4() {
 		Scanner s = new Scanner(System.in);
-		System.out.println("In: ");
+		System.out.print("In: ");
 		double num = s.nextDouble();	
-		if (num % -2 == 2) {
-			System.out.println("True");
+		if (num % 2 == 0 && num >= -2) {
+			System.out.println("true");
 		} else {
-			System.out.println("False");
+			System.out.println("false");
 		}
 		s.close();	
 	}
 
 	public static void q5() {
 		Scanner s = new Scanner(System.in);
-		System.out.println("In: ");
+		System.out.print("In: ");
 		String word = s.nextLine();
 		if (word.equals( "Hello World")) {
-			System.out.println("True");
+			System.out.println("true");
 		} else {
-			System.out.println("False");
+			System.out.println("false");
 		}
 		s.close();
 	}
 
 	public static void q6() {
 		Scanner s = new Scanner(System.in);	
-		System.out.println("In: ");
+		System.out.print("In: ");
 		int num = s.nextInt();
-		System.out.println("In: ");
+		System.out.print("In: ");
 		int num2 = s.nextInt();
 		if (num <= num2) {
-			System.out.println("True");
-		}	else {
-			System.out.println("False");
+			System.out.println("true");
+		}	 else {
+			System.out.println("false");
 		}
 		s.close();
 	}
 
 	public static void q7() {
 		Scanner s = new Scanner(System.in);
-		System.out.println("In: ");
+		System.out.print("In: ");
 		double num = s.nextDouble();
-		System.out.println("In: ");
+		System.out.print("In: ");
 		double num2 = s.nextDouble();
 		if (num > num2) {
-			System.out.println("True");
-		}	else {
-			System.out.println("False");
+			System.out.println("true");
+		}	 else {
+			System.out.println("false");
 		}
 		s.close();
 		}	
